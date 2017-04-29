f866a0c44383c6cd92524c7cf62e51ad999d3293
happysmash27 <happysmash27@vmail.me>
2017-04-29 14:40:24 -0700

    It's workinggit add -A! The server is ACTUALLY SERVING THE WEBSITEgit add -Agit add -A :D

---
+++ Server/Javascript Client.bak/index.js
@@ -0,0 +1,12 @@
+console.log("Okay, time to MAKE THIS WEBSITE!!!")
+
+function listener(){
+    console.log(this.responseText);
+}
+
+var request = new XMLHttpRequest();
+request.addEventListener("load", listener);
+request.open("GET", "http://127.0.0.1:8888");
+request.send();
+
+console.log("The code runs here!")
